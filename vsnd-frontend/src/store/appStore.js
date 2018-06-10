bc091695ccceca0f77a9f20a1d5a9decaaf53b41
daniel glenn <linuxbender@gmail.com>
2018-06-10 07:23:18 +0200

    feat: setup store

--- vsnd-frontend/src/store/appStore.js
+++ vsnd-frontend/src/store/appStore.js
@@ -1,9 +1,16 @@
 import {applyMiddleware, compose, createStore} from 'redux'
+import {loggerMiddleware} from '../middleware/loggerMiddleware';
+import {T_Store} from '../utils/appTypes';
+import rootReducer from '../reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = () => {
-    return {};
+    return createStore(
+        rootReducer,
+        T_Store,
+        composeEnhancers(applyMiddleware(...loggerMiddleware))
+    );
 };
 
 export default configureStore;
