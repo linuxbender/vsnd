9cc49fc1b66e4f0c9bdf2500362b1f46283eb6d7
daniel glenn <linuxbender@gmail.com>
2018-06-12 02:06:18 +0200

    feat: add search components to the project

--- vsnd-frontend/src/App.js
+++ vsnd-frontend/src/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import './App.css';
 import Button from './components/Base/Button';
 import Header from './components/Base/Header';
+import Search from './components/form/Search';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Header/>
+                <Header />
+                <Search />
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                     <Button/>
