24ee613fa1a1f4d941e655d89fffa220fd5a4c87
daniel glenn <linuxbender@gmail.com>
2018-06-15 01:32:33 +0200

    feat: i18n load translation to the app example

--- vsnd-frontend/src/index.js
+++ vsnd-frontend/src/index.js
@@ -5,7 +5,11 @@ import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/appStore';
+import message from './i18n/message.de.json';
 
 const store = configureStore();
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
+
+console.log(message);
+console.log(message.filter(i => i.key === 'global.title')[0].value);
